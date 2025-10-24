3347e78561468cdb78dd65b65b11a29822f1471e
soedomoto <erika.mukhlisina@gmail.com>
2025-10-25 00:09:25 +0100

    Add navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
 import '@mantine/core/styles.css';
-import { MantineProvider } from '@mantine/core';
+import { createTheme, localStorageColorSchemeManager, MantineProvider } from '@mantine/core';
+import { NavbarNested } from './NavbarNested/NavbarNested';
 
 export default function App() {
-  return <MantineProvider>
-    App
+  const theme = createTheme({
+    fontFamily: 'Open Sans, sans-serif',
+    primaryColor: 'cyan',
+  });
+
+  const colorSchemeManager = localStorageColorSchemeManager({
+    key: 'labelify-web-color-scheme',
+  });
+
+  return <MantineProvider theme={theme} colorSchemeManager={colorSchemeManager} classNamesPrefix="labelify">
+    <NavbarNested />
   </MantineProvider>;
 }
